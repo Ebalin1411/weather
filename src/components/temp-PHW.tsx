9c872a730af3a4b5dde92c3f63b75b1c6c85b5e5
Ebalin1411 <ebalinrosecse2006@gmail.com>
2022-11-07 22:14:49 +0530

    feat: local state added for TempUnit

--- src/components/temp-PHW.tsx
+++ src/components/temp-PHW.tsx
@@ -16,6 +16,8 @@ interface Props{
 export const TempPHW =({data}:Props) => {
     // console.log('Data comming as a Props from tempPHW Component', data)     
     const [weatherUnit,setWeatherUnit]=useState(1);
+    var tempIncelsius = data.temperature;
+    var tempInFahrenheit = Math.round(tempIncelsius * 9/5 +32);
 
     return <div className='flex gap-10'>
                 <div className=''>
@@ -23,8 +25,39 @@ export const TempPHW =({data}:Props) => {
                 </div>
                
                 <div className='flex text-2xl'>
-                    <span className='text-5xl font-bold'>{data.temperature}</span>
-                    <span>{data.temperatureUnits}°</span>
+                         <div className={weatherUnit === 1 ? "block" : "hidden"}>                           
+                            
+                         <span className='text-5xl font-bold'>{data.temperature}</span>
+                        </div>
+                        <div className={weatherUnit === 2 ? "block" : "hidden"}>
+                            <span className='text-5xl font-bold'>{tempInFahrenheit}</span>
+                        </div>
+                    <div>
+                         
+                    </div>
+                    <div className='flex'>
+                        <ul>
+                            <li>                               
+                                <a  href="#" className="inline-block" onClick={() => setWeatherUnit(1)}>
+                                   
+                                    C°
+                                </a>
+                                
+                            </li>
+                        </ul>
+                        <ul>
+                            <li>                               
+                                 <a href="#" className="inline-block" onClick={() => setWeatherUnit(2)}>
+                                
+                                    F°
+                                </a>                                
+                            </li>
+                        </ul>                     
+                       
+                    </div>
+
+                   
+
                 </div>
                 <div className='flex-col'>
                     <span className='block'>{data.precipitation}</span>
